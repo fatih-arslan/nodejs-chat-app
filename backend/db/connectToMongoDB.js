77103f7e9365600a58b7760afae048e9c2380430
Fatih Arslan <fatiharslan748@gmail.com>
2024-05-04 18:42:32 +0300

    remove unnecessary log

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const connectToMongoDB = async () => {
     try {
-        console.log(process.env.MONGO_DB_URI);
         mongoose.connect(process.env.MONGO_DB_URI);
         console.log('Connected to MongoDB');
     }
